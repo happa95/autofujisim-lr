b10a983ffb5c95f2f0ffd099526675008d44f5f8
Tyler Hakomori <tyler_hakomori@brown.edu>
2018-12-01 11:00:56 -0800

    Info menu, instead of library menu for ease of accessibility

--- AutoFujiSim.lrdevplugin/Info.lua
+++ AutoFujiSim.lrdevplugin/Info.lua
@@ -1,12 +1,12 @@
-return {
-    
+return { 
+
     LrSdkVersion = 7.0,
     LrSdkMinimumVersion = 3.0, 
     LrToolkitIdentifier = 'AutoFujiSim',
 
     LrPluginName = "AutoFujiSim",
     
-    LrLibraryMenuItems = {
+    LrInfoMenuItems = {
         title = "Apply film simulations",
         file = "AutoFujiSim.lua",
         enabledWhen = "photosSelected",
